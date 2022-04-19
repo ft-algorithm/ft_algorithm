b461ec0d4c8e2ff9a11e7d9a7573385ca0517325
reg0145 <90084199+reg0145@users.noreply.github.com>
2022-04-19 16:49:26 +0900

    Update is_valid_sudoku.c

--- donghyuk/LeetCode/is_valid_sudoku/is_valid_sudoku.c
+++ donghyuk/LeetCode/is_valid_sudoku/is_valid_sudoku.c
@@ -10,9 +10,9 @@ int check_line(char* line, int len, int dist)
 	{
 		if (*line == '.')
 			;
-        else if (table[(*line - '0')]++ > 0)
+		else if (table[(*line - '0')]++ > 0)
 			return (false);
-        line += dist;
+		line += dist;
 	}
 	free(table);
 	return (true);
@@ -20,47 +20,47 @@ int check_line(char* line, int len, int dist)
 
 int check_area(char **area, int row, int col, int boardSize)
 {
-    int *table;
-    int row_idx;
-    int col_idx;
+	int *table;
+	int row_idx;
+	int col_idx;
 
-    table = (int *)calloc(boardSize + 1, sizeof(int));
-    row_idx = -1;
-    while (row_idx++ < 2)
-    {
-        col_idx = -1;
-        while (col_idx++ < 2)
-		{
-			if (area[row + row_idx][col + col_idx] == '.')
-				;
-            else if(table[area[row + row_idx][col + col_idx] - '0']++ > 0)
-                return (false);
-		}
+	table = (int *)calloc(boardSize + 1, sizeof(int));
+	row_idx = -1;
+	while (row_idx++ < 2)
+	{
+	col_idx = -1;
+	while (col_idx++ < 2)
+	{
+		if (area[row + row_idx][col + col_idx] == '.')
+			;
+		else if(table[area[row + row_idx][col + col_idx] - '0']++ > 0)
+		return (false);
 	}
-    free(table);
+	}
+	free(table);
 	return (true);
 }
 
 bool isValidSudoku(char **board, int boardSize, int* boardColSize)
 {
-    int row;
-    int col;
+	int row;
+	int col;
 
-    row = 0;
-    while (row < boardSize)
-        if (check_line(&board[row++][0], boardSize, 1) == false)
-            return (false);
-    col = 0;
-    while (col < boardSize)
-        if (check_line(&board[0][col++], boardSize, board[1] - board[0]) == false)
-            return (false);
-    row = -1;
-    while (row++ < 2)
-    {
-        col = -1;
-        while (col++ < 2)
-            if (check_area(board, row * 3, col * 3, boardSize) == false)
-                return (false);
-    }
-    return (true);
+	row = 0;
+	while (row < boardSize)
+	if (check_line(&board[row++][0], boardSize, 1) == false)
+	    return (false);
+	col = 0;
+	while (col < boardSize)
+	if (check_line(&board[0][col++], boardSize, board[1] - board[0]) == false)
+	    return (false);
+	row = -1;
+	while (row++ < 2)
+	{
+	col = -1;
+	while (col++ < 2)
+	    if (check_area(board, row * 3, col * 3, boardSize) == false)
+		return (false);
+	}
+	return (true);
 }
